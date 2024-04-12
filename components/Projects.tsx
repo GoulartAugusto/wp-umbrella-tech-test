0e6e318cc61064f481dff26d0e6028d7418c9462
Augusto Goulart <93224568+GoulartAugusto@users.noreply.github.com>
2024-04-11 21:17:03 -0300

    Optimazed Project.tsx component, with interface, async/await and unique keys

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,22 +1,23 @@
 "use client";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { Tag } from "./Tag";
 
+// I've defined an interface Project to specify the type of each project in the project state.
+interface Project {
+  id: number;
+  name: string;
+  tags: string[];
+  url: string;
+  description: string;
+  userId: number;
+}
+
 export function Projects() {
-  const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState<Project[]>([]);
 
   // instad of chaining '.then()' method, the 'async/await' method is more concise and readable
-  {/*
-    const getProjects = () => {
-      fetch("http://localhost:3000/api/projects").then((response) =>
-      response.json().then((data) => {
-        setProjects(data);
-      })
-      );
-    };
-*/}
-
+  // This code uses async/await with fetch for cleaner asynchronous code
   const getProjects = async () => {
     try {
       const response = await fetch("http://localhost:3000/api/projects")
@@ -31,14 +32,15 @@ export function Projects() {
     getProjects();
   }, []);
 
+  // When mapping elements, the best approach is to use unique identifiers for 'key' prop. 'project.id' is more suitable
   return (
     <div className="space-y-2">
       {projects.map((project) => (
-        <div className="rounded p-2 bg-white">
+        <div className="rounded p-2 bg-white" key={project.id}>
           <div className="flex items-center gap-2">
-            {project.tags.map((tag, key) => {
-              return <Tag tag={tag} key={key} />;
-            })}
+            {project.tags.map((tag) => (
+              <Tag tag={tag} key={`${project.id}`} />
+            ))}
           </div>
           <p>
             (Id: {project.id}) Name: {project.name}
