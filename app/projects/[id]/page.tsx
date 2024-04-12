5d04e1be9f67bc81883789422aefb87c2c8072b5
Augusto Goulart <93224568+GoulartAugusto@users.noreply.github.com>
2024-04-11 21:43:25 -0300

    added interface, and async/await logic to /projects/[id]/page.tsx

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,28 +1,48 @@
 "use client";
-import { Tag } from "components/Tag";
 import { useEffect, useState } from "react";
+import { Tag } from "components/Tag";
 
-export default function Home({ params }: { params: any }) {
-  const [project, setProject] = useState<{
-    id: number;
-    name: string;
-    description: string;
-    tags: string[];
-  } | null>(null);
+// I added interface to components to avoid potential errors.
+interface Project  {
+  id: number;
+  name: string;
+  description: string;
+  tags: string[]
+}
 
-  function getProject() {
-    fetch(`http://localhost:3000/api/projects/${params.id}`).then((response) =>
-      response.json().then((data) => {
-        setProject(data);
-      })
-    );
+interface Props {
+  params: {
+    id: string;
+  }
+}
+
+export default function Home({ params }: Props) {
+  // Now setProject state uses the interface Project
+  const [project, setProject] = useState<Project | null>(null);
+  // I added a loading state while waiting for the project data to fetch
+  const [loading, setLoading] = useState(true);
+
+  // instad of chaining '.then()' method, the 'async/await' I used async/await 
+  // for cleaner asynchronous code in the getProject function.
+  async function getProject() {
+    try {
+      const response = await fetch(`http://localhost:3000/api/projects/${params.id}`)
+      const data = await response.json()
+      setProject(data)
+      setLoading(false)
+    } catch (error) {
+      console.error("Something went wrong fetching this project:", error)
+      setLoading(false)
+    }
   }
 
   useEffect(() => {
     getProject();
   }, []);
 
-  if (!project) return <div>Loading...</div>;
+
+  if (loading) return <div>Loading...</div>;
+  if (!project) return <div>Project not found</div>
 
   return (
     <div className="bg-slate-50 p-4 w-full h-full">
